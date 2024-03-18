339dbc62742fbc10e2caf7493da8cb999f1621cc
unknown <sarat.pint@gmail.com>
2024-03-18 09:58:48 +0700

    2:deployment

--- middleware.ts
+++ middleware.ts
@@ -21,14 +21,14 @@ export default auth((req) => {
     const isAuthRoute = authRoutes.includes(nextUrl.pathname);
 
     if(isApiAuthRoute){
-        return null;
+        return;
     }
 
     if(isAuthRoute){
         if(isLoggedIn){
             return Response.redirect(new URL(DEFAULT_LOGIN_REDIRECT,nextUrl));
         }
-        return null;
+        return;
     }
 
     if(!isLoggedIn && !isPublicRoute){
@@ -45,7 +45,7 @@ export default auth((req) => {
         return Response.redirect(new URL(`/auth/login?callbackUrl=${encodedCallbackUrl}`,nextUrl));
     }
 
-    return null;
+    return;
 })
 
 // Optionally, don't invoke Middleware on some paths
